2761db2632af93a619b17c763971eb3498207112
alfaville <lipealfaville@gmail.com>
2015-02-02 00:26:35 -0300

    init

--- app/models/Usuario.java
+++ app/models/Usuario.java
@@ -11,6 +11,7 @@ import play.data.validation.Constraints.Required;
 import play.data.validation.ValidationError;
 import play.i18n.Messages;
 import play.libs.Crypto;
+import play.mvc.Http.Session;
 import validacao.GrupoValidaLogin;
 import play.db.ebean.Model.Finder;
 
@@ -32,23 +33,30 @@ public class Usuario extends EntidadeBase {
 	public String senha;
 
 	@Transient
-	public Projetos projeto = new Projetos();
+	@Required(groups = GrupoValidaLogin.validaLogin.class, message = "Projeto é obrigatório")
+	public String projeto;
 	
 	@ManyToMany(mappedBy = "usuario")
 	public List<Projetos> projetos;
 	
 	public static Finder<Long, Usuario> find = new Finder<Long, Usuario>(Long.class, Usuario.class);
 	
-	public List<ValidationError> validate() {
+	public List<ValidationError> validate() {		
 		List<ValidationError> errors = new ArrayList<ValidationError>();		
 		Usuario usuario = Usuario.find.where().eq("login", login).eq("senha", Crypto.sign(senha)).findUnique();
 		if ( usuario == null )
 			errors.add(new ValidationError("usuario", Messages.get("loginView.incorreto")));
-		else if ( usuario.projeto == null )
+		else if ( this.projeto == null )
 			errors.add(new ValidationError("projeto", Messages.get("loginView.projeto.erro")));
-		else if (!usuario.projetos.contains(projeto))
+		else {
+			List<Projetos> listProjetos = usuario.projetos;
+			for (Projetos p : listProjetos) {
+				if ( p.nome.equals(this.projeto) )
+					return null;
+			}
 			errors.add(new ValidationError("projeto.nao.inexiste", Messages.get("loginView.projeto.erro.inexistente")));
-		
+		}
+			
 		return errors.isEmpty() ? null : errors;
 	}
 	
